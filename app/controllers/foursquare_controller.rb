6f0435db3f096be79846e482528e9b7f30dcb05a
Rachelle Keblitis <rkeblitis@gmail.com>
2015-02-03 16:41:04 -0800

    make individual_info() return on random photo

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -1,27 +1,46 @@
 class FoursquareController < ApplicationController
 
   def index
-  @response = HTTParty.get("https://api.foursquare.com/v2/venues/search?ll=47.624784,-122.356922&radius=500&categoryId=4d4b7105d754a06374d81259&client_id=#{ENV["FOURSQUARE_CLIENT_ID"]}&client_secret=#{ENV["FOURSQUARE_CLIENT_SECRET"]}&v=20150126")
-  # render json: @response.parsed_response["response"]["venues"]
-  ids = []
-  @response.parsed_response["response"]["venues"].each do |venue|
-    ids << venue["id"]
+  @venue_response = HTTParty.get("https://api.foursquare.com/v2/venues/search?ll=47.624784,-122.356922&radius=500&categoryId=4d4b7105d754a06374d81259&client_id=#{ENV["FOURSQUARE_CLIENT_ID"]}&client_secret=#{ENV["FOURSQUARE_CLIENT_SECRET"]}&v=20150126")
+  venue_ids = []
+  @venue_response.parsed_response["response"]["venues"].each do |venue|
+    venue_ids << venue["id"]
   end
-  individual_info(ids)
+  individual_info(venue_ids)
   end
 
-  def individual_info(ids)
-    @pics = []
-    ids.each do |id|
-      id_response = HTTParty.get("https://api.foursquare.com/v2/venues/#{id}/photos?&client_id=#{ENV["FOURSQUARE_CLIENT_ID"]}&client_secret=#{ENV["FOURSQUARE_CLIENT_SECRET"]}&v=20150126")["response"]["photos"]["items"]
-      id_response.each do |pic_info|
-        prefix = pic_info["prefix"]
-        suffix = pic_info["suffix"]
-        photo_url = prefix + "400x400" + suffix
-        @pics << photo_url
-      end
+  def individual_info(venue_ids)
+    id = venue_ids.sample
+    @pic = []
+    id_response = HTTParty.get("https://api.foursquare.com/v2/venues/#{id}/photos?&client_id=#{ENV["FOURSQUARE_CLIENT_ID"]}&client_secret=#{ENV["FOURSQUARE_CLIENT_SECRET"]}&v=20150126")["response"]["photos"]["items"]
+    id_response.each do |pic_info|
+      prefix = pic_info["prefix"]
+      suffix = pic_info["suffix"]
+      photo_url = prefix + "400x400" + suffix
+      @pic << photo_url
     end
-    render json: @pics
+    respond_to do |format|
+       format.html {render html: @pic.sample }
+       
+    end
+
   end
 
 end
+
+
+# seperate my api so that noms can make a call and get one picture at a time
+# only query a venue id when needed to help optimization
+
+
+# @pics = []
+# venue_ids.each do |id|
+#   id_response = HTTParty.get("https://api.foursquare.com/v2/venues/#{id}/photos?&client_id=#{ENV["FOURSQUARE_CLIENT_ID"]}&client_secret=#{ENV["FOURSQUARE_CLIENT_SECRET"]}&v=20150126")["response"]["photos"]["items"]
+#   id_response.each do |pic_info|
+#     prefix = pic_info["prefix"]
+#     suffix = pic_info["suffix"]
+#     photo_url = prefix + "400x400" + suffix
+#     @pics << photo_url
+#   end
+# end
+# render json: @pics
